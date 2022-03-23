a92b182d2d97875fb23d77805b8608e4c71ea2b1
Al Ki Lee <al_tanner2012@hotmail.com>
2022-03-23 16:22:42 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PokemonsComponent } from './components/pokemons/pokemons.component';
+import { GenerationsComponent } from './components/generations/generations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonsComponent,
+    GenerationsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/generations/generations.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-generations',
+  templateUrl: './generations.component.html',
+  styleUrls: ['./generations.component.sass']
+})
+export class GenerationsComponent implements OnInit {
+  private _generations: string[];
+
+  constructor() {
+    this._generations = [
+      'Génération I',
+      'Génération II',
+      'Génération III',
+      'Génération IV',
+      'Génération V',
+      'Génération VI',
+      'Génération VII',
+    ]
+  }
+
+  get generations(): string[] {
+    return this._generations;
+  }
+
+  set generations(value: string[]) {
+    this._generations = value;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
